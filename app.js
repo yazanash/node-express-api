89b2fd6b70dba8c2e04ed578e25932cdac2c3336
yazanash <yazan.ash.doonaas@gmail.com@gmail.com>
2023-09-29 14:38:25 +0300

    setup project 4

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.get("/hello",(req,res)=>{
     res.send("Hello World from express js")
 })
 
-
+app.get("/home",(req,res)=>{
+    res.send("Welcome from home")
+})
+ 
 app.listen(3000,()=>{
     console.log("app started on prot 3000")
 })
